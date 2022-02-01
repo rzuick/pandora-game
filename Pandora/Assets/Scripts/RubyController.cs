117c7f2a8055fb731be7f1f437ec6844222632e3
Rebecca Zuick <rebeccazuick@Rebeccas-Air.attlocal.net>
2022-01-31 21:07:52 -0600

    added camera follow functionality. added rigidbody component to player

--- Pandora/Assets/Scripts/RubyController.cs
+++ Pandora/Assets/Scripts/RubyController.cs
@@ -15,12 +15,10 @@ public class RubyController : MonoBehaviour
     {
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
-        float jump = Input.GetAxis("Jump");
 
         Vector2 position = transform.position;
         position.x = position.x + 3.0f * horizontal * Time.deltaTime;
         position.y = position.y + 3.0f * vertical * Time.deltaTime;
-        position.y = position.y + 3.0f * jump * Time.deltaTime;
         transform.position = position;
     }
 }
