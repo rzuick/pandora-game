f8ac9f26f287a497f997d3c87f76ffa3d843627c
Rebecca Zuick <rebeccazuick@Rebeccas-Air.attlocal.net>
2022-02-14 13:25:00 -0600

    refactored loadText to make it more scalable. edited scenes to allow for smoother transitions between scenes

--- Pandora/Assets/Scripts/LoadText.cs
+++ Pandora/Assets/Scripts/LoadText.cs
@@ -12,6 +12,7 @@ public class LoadText : MonoBehaviour
     public Animator transition;
     public bool stillTyping = true;
     public int levelToLoad;
+    public float startTransition;
     void Start()
     {
         storyText = GetComponent<TMP_Text>();
@@ -40,7 +41,7 @@ public class LoadText : MonoBehaviour
      IEnumerator EndGame () 
     {
         transition.SetTrigger("Start");
-        yield return new WaitForSeconds(2.0f);
+        yield return new WaitForSeconds(startTransition);
         SceneManager.LoadScene(levelToLoad);
     }
 }
